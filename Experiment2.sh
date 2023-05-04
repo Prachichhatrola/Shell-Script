179ec5c0cfd6dee0baf2658564a2df4e11157f52
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-04 14:21:53 +0530

    Update Experiment2.sh

--- Experiment2.sh
+++ Experiment2.sh
@@ -1,5 +1,5 @@
 # AIM : Write a command to display a message hello I am leraning operating system on the terminal.
---> echo hello I am learning operating system 0n the terminal
+--> echo hello I am learning operating system on the terminal
 
 # AIM : Write a command to display comment and current time on terminal window using echo command.
 --> # Hello !
