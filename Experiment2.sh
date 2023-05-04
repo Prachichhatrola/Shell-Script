3cb5b2939009dc61960f8a3252e142f90f52f0d2
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-04 08:47:42 +0530

    Add files via upload

---
+++ Experiment2.sh
@@ -0,0 +1,16 @@
+# AIM : Write a command to display a message hello I am leraning operating system on the terminal.
+--> echo hello I am learning operating system 0n the terminal
+
+# AIM : Write a command to display comment and current time on terminal window using echo command.
+--> # Hello !
+--> Date "+Time : %T"
+
+# AIM : Write a command to create a directory named in thedefault directory of current user on terminal window.
+--> mkdir default
+    cd default
+    mkdir demo
+    
+# AIM :  Write a command to create a file named GECBVN and display the contents of it.
+--> touch GECBVN
+    Hello !
+    cat ./GECBVN
