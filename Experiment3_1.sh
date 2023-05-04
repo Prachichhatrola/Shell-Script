250f249b7e1541b47eb9a702d794a29ecb65f3d3
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-04 21:45:32 +0530

    Add files via upload

---
+++ Experiment3_1.sh
@@ -0,0 +1,27 @@
+echo "Marksheet of students"
+echo "Enter the Marks of Oops subject: "
+read oops
+echo "Enter the Marks of Operating System: "
+read os
+echo "Enter the Marks of COA: "
+read coa
+
+echo "Total marks : " $(($oops + $os + $coa))
+echo "percentage : " $((($oops + $os +$coa) / 3))
+
+if [ $((($oops + $os $coa) / 3)) -ge 80 ]
+then 
+echo "You get Distinction"
+
+elif [ $((($oops + $os + $coa) / 3)) -ge 60 ]
+then 
+echo "You get First class"
+
+elif [ $((($oops + $os + $coa) / 3)) -ge 40 ]
+then 
+echo "YOu get Second class"
+
+else
+echo "Sorry! you get fail"
+fi
+
