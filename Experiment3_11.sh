0819816a6b6e6429839f1eb6ecbc3b7d46f019c6
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-05 20:10:27 +0530

    Add files via upload

---
+++ Experiment3_11.sh
@@ -0,0 +1,21 @@
+echo "Main Menu"
+echo "Press 1 for using Grep command"
+echo "Press 2 for using Egrep Command"
+echo "Press 3 for using Fgrep Command"
+read a
+case $a in
+1) echo "For single pattern search, Enter Pattern below :"
+read b
+grep "$b" City_names.txt
+;;
+2) echo "For double Pattern search, Enter b, c pattern :"
+read b
+read c
+egrep "$b" City_names.txt
+grep -E "$c"City_names.txt
+;;
+3) echo "For Pattern From a File, Enter Pattern :"
+read b
+grep -F "$b" City_names.txt
+;;
+esac
