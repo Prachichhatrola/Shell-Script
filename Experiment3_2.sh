f63b63c3368a5e34487dc7611cfb93702ba58e0a
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-05 08:33:43 +0530

    Add files via upload

---
+++ Experiment3_2.sh
@@ -0,0 +1,11 @@
+echo "Sum of 4 numbers"
+echo "Enter 1st number: "
+read a
+echo "Enter 2nd number: "
+read b
+echo "Enter 3rd number: "
+read c
+echo "Enter 4th number: "
+read d
+echo result = $(($a + $b + $c + $d))
+echo "Your answer is: "$(($a + $b + $c + $d))
