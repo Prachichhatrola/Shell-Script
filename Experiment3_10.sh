a9e34934cf9df6511b18d7fe90ea2ddf7c510f3c
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-05 19:58:35 +0530

    Add files via upload

---
+++ Experiment3_10.sh
@@ -0,0 +1,13 @@
+echo "Enter a String: "
+read string
+length=${#string}
+for ((i=$length-1; i>=0; i--))
+do
+reverse="$reverse${string:$i:1}"
+done
+if [ $string == $reverse ]
+then
+echo "'$string' string is palindrome"
+else
+echo"'$string' string is not palindrome"
+fi
