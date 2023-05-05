8bef2b31d5fc8ec5ae9170b859707459a38236ca
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-05 19:37:08 +0530

    Add files via upload

---
+++ Experiment3_8.sh
@@ -0,0 +1,21 @@
+$ cat t.sh
+#!/bin/sh
+read -p "Enter The Number: " n
+for((i=0; i<$n; i++))
+do
+read -p "Enter value of arr[$i]: " arr[$i]
+done
+#sorting code
+for((i=0; i<$n; i++))
+do
+for((j=0; j<n-i-1; j++))
+do
+if [ ${arr[j]} -lt ${arr[$((j+1))]} ]
+then
+#swapping
+temp=${arr[j]}
+arr[$j]=${arr[$((j+1))]}
+arr[$((j+1))]=$temp
+fi
+done
+echo "Number in Descending order: " ${arr[*]}
