f33b8c6bade142117bee5bd5383657d98992f076
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-06 11:43:20 +0530

    Update Experiment3_7.sh

--- Experiment3_7.sh
+++ Experiment3_7.sh
@@ -1,3 +1,10 @@
+#AIM : Write a menu driven shell script which will print the following menu and execute the given task.
+#a. Display calender the given task.
+#b. Display today's date and time.
+#c. Display username those are currently logged in the system.
+#d. Display your name at given x,y position.
+#e. Display your terminal number.
+
 echo "Select Anyone Option"
 echo "***************************************************************"
 echo "1) Display Calender of current month"
