21bcd7739c0e3fd8a053790689b8852f11beaa7e
Prachi Chhatrola <prachichhatrola009@gmail.com>
2023-05-07 08:09:48 +0530

    Add files via upload

---
+++ Experiment4_3.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+for((i=3; i>=1; i--))
+do
+for((j=1; j<=3; j++))
+do
+if [ $j -ge $i ]
+then
+echo -n "* "
+else
+echo -n " "
+fi
+done
+echo
+done
+for((i=1; i<=3; i++))
+do
+for((j=3; j>=1; j--))
+do
+if [ $i -ge $j ]
+then
+echo -n " "
+else
+echo -n " *"
+fi
+done
+echo
+echo -n " "
+done 
